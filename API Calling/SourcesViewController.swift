8bffe6358a02ae15e52c2ade603dea54302ceb4b
Sydney Ripsky <sripsky@bsd220.org>
2020-02-06 10:36:02 -0600

    Parse JSON data

--- API Calling/SourcesViewController.swift
+++ API Calling/SourcesViewController.swift
@@ -9,14 +9,38 @@
 import UIKit
 
 class SourcesViewController: UITableViewController {
-     var sources = [[String: String]]()
+    var properties = [[String: String]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Star Wars Starships"
+        self.title = "Properties"
         let query = "https://swapi.co/api/starships/schema"
+        if let url = URL(string: query) {
+             if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                   parse(json: json)
+                   return
+                }
+          }
+          loadError()
     }
-
-
+    func parse(json: JSON) {
+        for result in json["properties"].arrayValue {
+            let name = result["name"].stringValue
+            let length = result["length"].stringValue
+            let passengers = result["passenger"].stringValue
+            let pilots = result["pilots"].stringValue
+            let property = ["name" : name, "length" : length, "passengers" : passengers, "pilots": pilots]
+            properties.append(property)
+        }
+        tableView.reloadData()
+    }
+    func loadError() {
+         let alert = UIAlertController(title: "Loading Error",
+                                     message: "There was a problem loading the news feed",
+                              preferredStyle: .actionSheet)
+         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+         present(alert, animated: true, completion: nil) }
 }
+
 
